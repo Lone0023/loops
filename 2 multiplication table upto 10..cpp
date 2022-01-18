9bc396878853282a22255eda34f8f84ec8e7af1f
Shaswat SInha <97851942+Lone0023@users.noreply.github.com>
2022-01-18 10:23:47 +0530

    Add files via upload

---
+++ 2 multiplication table upto 10..cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Enter the value of a: "<<endl;
+    cin>>a;
+    while(b<=10){
+        cout<<a<<" * "<<b<<" = "<<a*b<<endl;
+        b++;
+    }
+    return 0;
+
+}
